6e60367f227a909155678ba98819feac35d2d3b9
Martin Zalabak <mana.zalabak@gmail.com>
2019-10-08 18:47:03 +0200

    add license note into completion script

--- bunit_completion.sh
+++ bunit_completion.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+################################################################################
+#
+# Copyright 2019 Martin Zalabak
+#
+# This file is part of bunit.
+#
+# bunit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# bunit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with bunit.  If not, see <https://www.gnu.org/licenses/>.
+#
+################################################################################
+
 _comp_bunit ()
 {
     local scriptname=$1
